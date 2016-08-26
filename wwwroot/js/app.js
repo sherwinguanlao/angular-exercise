face4145e4e4e62ff81e28122e453e296ce82446
sherwinguanlao <guanlao.sherwin@gmail.com>
2016-08-27 01:21:32 +0800

    Add files via upload

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -81,22 +81,31 @@
 (function () {
 	angular
 		.module('DirectorySearchApplication')
-		.controller('DetailsController', ['$scope', '$routeParams', 'ArtistService', function ($scope, $routeParams, ArtistService) {
+		.controller('DetailsController', ['$scope', '$routeParams', 'ArtistService', '$location', function ($scope, $routeParams, ArtistService, $location) {
 			$scope.index = parseInt($routeParams.index);
+			$scope.firstArtist = false;
+			$scope.lastArtist = false;
 
-			$scope.prevFunction = function (x) {
+			if($scope.index == 0)
+				$scope.firstArtist = true;
+			
+			$scope.prevFunction = function () {
 				if ($scope.index == 0) {
-					$scope.index = x - 1;
+					$scope.firstArtist = true;
 				} else {
+					$scope.firstArtist = false;
 					$scope.index = $scope.index - 1;
+					$location.path('/detail/'+ $scope.index);
 				}
 			};
 
-			$scope.nextFunction = function (x) {
-				if ($scope.index == x - 1) {
-					$scope.index = 0;
+			$scope.nextFunction = function () {
+				if ($scope.index == $scope.artists.length - 1) {
+					$scope.lastArtist = true;
 				} else {
+					$scope.lastArtist = false;
 					$scope.index = $scope.index + 1;
+					$location.path('/detail/'+ $scope.index);
 				}
 			};
 
@@ -104,6 +113,8 @@
 			ArtistService.getArtistDetails()
 				.then(function (response) {
 					$scope.artists = response.data;
+					if($scope.index == $scope.artists.length - 1)
+						$scope.lastArtist = true;
 				});
 		}]);
 })();
